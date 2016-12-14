be7d0bba3b71620439ad0d7535091b194f331023
Jeffrey Bodin <jeffreybodin713@gmail.com>
2016-12-13 23:39:08 -0600

    Add files via upload

---
+++ My first ruby code academy project 12_13_16.rb
@@ -0,0 +1,20 @@
+print "Enter First Name:"
+    first_name = gets.chomp
+    first_name.capitalize!
+    
+print "Enter Last Name:"
+    last_name = gets.chomp
+    last_name.capitalize!
+
+print "Enter City:"
+    city_name = gets.chomp
+    city_name.capitalize!
+
+print "Enter state abbreviation (NY = New York ...etc):"
+    state_name = gets.chomp
+    state_name.upcase!
+
+puts "First Name: #{first_name}"
+    puts "Last Name: #{last_name}"
+    puts "City: #{city_name}"
+    puts "State: #{state_name}"
