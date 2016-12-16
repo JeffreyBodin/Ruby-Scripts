be2b95be2d398a87be412e4228aea9e6eb57a467
Jeffrey Bodin <jeffreybodin713@gmail.com>
2016-12-16 01:30:59 -0600

    Added a .times iterator example for printing "Ruby!" 30 times.

--- loops.rb
+++ loops.rb
@@ -9,3 +9,9 @@ loop do
 end
 # Uses a variable "a" assigned as a counter to allow the script to break at 30 counters.
 # Therefore only allowing 30 printings of "Ruby!"
+
+# This example is a reconfiguration of the above loop.
+30.times do 
+print "Ruby!" 
+end
+# Here .times iterator is used instead of a loop to print 30 times.
