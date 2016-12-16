2132b0e2061de7ec409ef58a30bb76630408502b
Jeffrey Bodin <jeffreybodin713@gmail.com>
2016-12-16 00:34:16 -0600

    Fixed error correctly counts to one hundred now. Previously counted indefinitely b/c was set while 1 < 101. Of course 1 will always be less than 101 so it continued to execute the script and count indefintely. It now correctly states that while count_to_hundred < 101 it is to puts count_to_hundred until it does in fact count_to_hundred. This was my first error in a script and my first bugfix. 12/16/2016 - 12:31 am

--- counts to one hundred.rb
+++ counts to one hundred.rb
@@ -1,7 +1,15 @@
 count_to_hundred = 1
 
-while 1 < 101
+while count_to_hundred < 101
   puts count_to_hundred
   count_to_hundred = count_to_hundred + 1
 end
 # Counts to one hundred from 1. A simple counting script!
+
+=begin
+Fixed error correctly counts to one hundred now.
+Previously counted indefinitely b/c was set while 1 < 101.
+Of course 1 will always be less than 101 so it continued to execute the script and count indefintely.
+It now correctly states that while count_to_hundred < 101 it is to puts count_to_hundred until it does in fact count_to_hundred.
+This was my first error in a script and my first bugfix. 12/16/2016 - 12:31 am
+end
