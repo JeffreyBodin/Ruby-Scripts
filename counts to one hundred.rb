be8422832a8ee4b43b613a56e7eba5a097a67944
Jeffrey Bodin <jeffreybodin713@gmail.com>
2016-12-16 00:47:46 -0600

    Added counts to 10 script using += assignment operators

--- counts to one hundred.rb
+++ counts to one hundred.rb
@@ -12,4 +12,13 @@ Previously counted indefinitely b/c was set while 1 < 101.
 Of course 1 will always be less than 101 so it continued to execute the script and count indefintely.
 It now correctly states that while count_to_hundred < 101 it is to puts count_to_hundred until it does in fact count_to_hundred.
 This was my first error in a script and my first bugfix. 12/16/2016 - 12:31 am
+=end
+
+##The rest of this script will be counting reference scripts. ##
+
+#Counts to 10 script. Shows another way to count using the += assignment operator.
+counter = 1   
+while counter <= 10
+  puts counter
+  counter += 1
 end
