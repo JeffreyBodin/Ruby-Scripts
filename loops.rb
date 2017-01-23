e0c5df7aa3b86c3b14b52a82b11a386916c96450
Jeffrey Bodin <jeffreybodin713@gmail.com>
2017-01-23 15:13:05 -0600

    notes

--- loops.rb
+++ loops.rb
@@ -15,3 +15,10 @@ end
 print "Ruby!" 
 end
 # Here .times iterator is used instead of a loop to print 30 times.
+
+#Iterating over loops/hashes Notes
+numbers = [1, 2, 3, 4, 5]
+numbers.each { |element| puts element }
+#In the example above, we create an array called numbers with 5 elements.
+#Then we say, "Take this array and for each element, print it to the console." 
+#We can use any placeholder name for the bit between two | | characters.
